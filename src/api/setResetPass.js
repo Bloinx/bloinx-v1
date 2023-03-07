153c570955123e8023d9b7c8bf70456eb452cd19
fiorellasaro <fiorellasaro27@gmail.com>
2023-03-06 21:42:00 -0500

    update validation with prod variable

--- src/api/setResetPass.js
+++ src/api/setResetPass.js
@@ -1,9 +1,10 @@
 import supabase from "../supabase";
 
 const ResetPass = async ({ emailReset, onSuccess, onFailure }) => {
-  const currentDomain = process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
-    : `http://${window.location.hostname}:3000`;
+  const currentDomain =
+    process.env.NODE_ENV === "production"
+      ? `https://${process.env.VERCEL_URL}`
+      : `http://${window.location.hostname}:3000`;
 
   const redirectToEmail = `${currentDomain}/update-password`;
 
