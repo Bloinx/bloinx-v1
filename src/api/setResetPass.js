7783a0354244354c02fa2b10fa9ed41bb236d368
fiorellasaro <fiorellasaro27@gmail.com>
2023-03-14 17:55:05 -0500

    react app domain for prod and dev

--- src/api/setResetPass.js
+++ src/api/setResetPass.js
@@ -2,7 +2,8 @@ import supabase from "../supabase";
 
 const ResetPass = async ({ emailReset, onSuccess, onFailure }) => {
   const currentDomain =
-    process.env.NODE_ENV === "production"
+    process.env.NODE_ENV === "production" ||
+    process.env.NODE_ENV === "development"
       ? // ? `https://${process.env.VERCEL_URL}`
         `https://${process.env.REACT_APP_DOMAIN}`
       : `http://${window.location.hostname}:3000`;
