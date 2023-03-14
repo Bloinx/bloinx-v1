6354cc4d3d659fbd71b4560530be80e42350d7db
fiorellasaro <fiorellasaro27@gmail.com>
2023-03-14 17:05:19 -0500

    add domain un url settings supabase

--- src/api/setResetPass.js
+++ src/api/setResetPass.js
@@ -2,12 +2,14 @@ import supabase from "../supabase";
 
 const ResetPass = async ({ emailReset, onSuccess, onFailure }) => {
   const currentDomain =
-    process.env.NODE_ENV === "production"
-      ? `https://${process.env.VERCEL_URL}`
+    process.env.NODE_ENV !== "production"
+      ? // ? `https://${process.env.VERCEL_URL}`
+        `https://${process.env.REACT_APP_DOMAIN}`
       : `http://${window.location.hostname}:3000`;
 
   const redirectToEmail = `${currentDomain}/update-password`;
-
+  console.log(`https://${process.env.VERCEL_URL} vercel-url`);
+  console.log("Redirect to:", redirectToEmail);
   const { data, error } = await supabase.auth.api.resetPasswordForEmail(
     emailReset,
     {
@@ -15,7 +17,8 @@ const ResetPass = async ({ emailReset, onSuccess, onFailure }) => {
     }
   );
   onSuccess(data);
-  console.log(error.message);
+  console.log("Password reset email sent successfully:", data);
+  console.log("Request headers:", error.response.headers);
   onFailure(error);
 };
 
